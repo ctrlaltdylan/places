2e1706873f888c8fa1bea0cc6feddda21014109b
Dylan Pierce <dylanpierce914@gmail.com>
2019-05-23 17:08:37 -0700

    Adding scrollspy and updating images locations

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 var port = process.env.PORT || 3000;
 
 
+const isDirectory = source => lstatSync(source).isDirectory()
+
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/public', express.static(__dirname + '/public'));
 // respond with "hello world" when a GET request is made to the homepage
@@ -13,13 +15,22 @@ app.get('/', function (req, res) {
 });
 
 app.get('/api/images', function(req, res) {
-  fs.readdir(path.join(__dirname, 'img'), function (err, files) {
 
-    res.send(JSON.stringify(files.filter(function(file) {
-      var parts = file.split('.');
-      return parts[1] == 'jpg';
-    })));
-  });
+  // for getting all albums and not caring about order 
+  // fs.readdir(path.join(__dirname, 'img/storyline'), function (err, albums) {
+    // var albums = albums.filter((album) => album != '.DS_Store')
+  var albums = ['seattle, wa', 'astoria, oregon', 'cannon beach, oregon', 'cape meares, oregon'];
+
+    var json = albums.reduce(function(albums, album) {
+        var images = fs.readdirSync(path.join(__dirname, 'img/storyline', album), function(err, files) {
+          return files.map((file) => path.join(__dirname, 'img/storyline', file));
+        });
+
+        return albums.concat({ album, images });
+      }, []);
+
+    res.send(JSON.stringify(json));
+  // });
 });
 
 
